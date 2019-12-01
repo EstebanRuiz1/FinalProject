df6d8ba735f7d3df0a645efd6ef73fce22b18eb0
EstebanRuiz1 <eduardeste15@gmail.com>
2019-12-01 12:05:25 -0500

    matriz creada y lista para recibir datos filas = corrales Columnas  = pollos por corral

--- src/models/Avicola.java
+++ src/models/Avicola.java
@@ -1,5 +1,21 @@
 package models;
 
 public class Avicola{
-	
+
+	public static final int MAXIUM_CORRAL_FILL = 300;
+	public static final int MAX_CORRAL_NUM = 10;
+	private int [][] corralChicken = new int [MAX_CORRAL_NUM][MAXIUM_CORRAL_FILL]
+
+	public Avicola(){
+
+/*	public int fillCorrals(int option){
+		if (option == 1) {
+			for (int i = 0;corralOne[i]< ; ) {
+				
+			}
+			
+		}
+	}
+
+*/	
 }
